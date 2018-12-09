45e3e7841166fd2fdf05c83b1dee95f416e5c86d
Alireza <a.modirshanechi@gmail.com>
2018-12-09 14:35:28 +0100

    Surprise version of data 2

--- Getting_Familiar_with_Surprise.py
+++ Getting_Familiar_with_Surprise.py
@@ -6,10 +6,8 @@ Created on Sun Dec  9 13:22:39 2018
 @author: alireza
 """
 # %% Load Packages
-from surprise import SVD
 from surprise import Dataset
 from surprise import Reader
-from surprise.model_selection import cross_validate
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,13 +16,13 @@ import utils
 
 # %% Test the package code
 # Load the movielens-100k dataset (download it if needed),
-data_ml100k = Dataset.load_builtin('ml-100k')
+#data_ml100k = Dataset.load_builtin('ml-100k')
 
 # We'll use the famous SVD algorithm.
-algo = SVD()
+#algo = SVD()
 
 # Run 5-fold cross-validation and print results
-cross_validate(algo, data_ml100k, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+#cross_validate(algo, data_ml100k, measures=['RMSE', 'MAE'], cv=5, verbose=True)
 
 # %% Load Project Dataset
 Path = "Data/data_train.csv"
@@ -34,5 +32,6 @@ Data_train = utils.load_data(Path)
 # path to dataset file
 file_path = "Data/data_train_Surprise_format.csv"
 
-reader = Reader(line_format='item user rating', sep=',',)
+reader = Reader(line_format='item user rating', sep=',',rating_scale=(1,5),
+                skip_lines=1)
 data = Dataset.load_from_file(file_path, reader=reader)
