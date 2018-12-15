ba74109efaf329fd34d172431a3e2caf8e90713c
Alireza <a.modirshanechi@gmail.com>
2018-12-15 16:14:46 +0100

    Removing the previous change

---
+++ Preprocessing.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 15 15:15:05 2018
+
+@author: alireza
+"""
+
+
+# %% Load Packages
+import random
+import numpy as np
+import utils
+
+# %% Random Seed
+my_seed = 0
+random.seed(my_seed)
+np.random.seed(my_seed)
+
+# %% Delete Outliers
+file_path = "Data/data_train.csv"
+save_path01 = "Data/data_train_preprocessed_50.csv"
+
+labels = utils.delete_users(file_path, min_num_items=50,
+                      save_path=save_path01)
+
+# %% Save in the Surprise format
+save_path02 = "Data/data_train_preprocessed_surprise_format_50.csv"
+utils.convert_to_surprise_format(
+        load_path=save_path01,
+        save_path=save_path02)
--- utils.py
+++ utils.py
@@ -46,11 +46,11 @@ def preprocess_data(data):
 
 # %% Load data in a desirable form for Surprise
 def deal_line(line):
-        pos, rating = line.split(",")
-        row, col = pos.split("_")
-        row = row.replace("r", "")
-        col = col.replace("c", "")
-        return int(row), int(col), float(rating)
+    pos, rating = line.split(",")
+    row, col = pos.split("_")
+    row = row.replace("r", "")
+    col = col.replace("c", "")
+    return int(row), int(col), float(rating)
 
 
 def load_data_desired(path_dataset):
@@ -75,11 +75,14 @@ def convert_to_surprise_format(load_path, save_path='surprise_data.csv'):
 
 # preprocessing data, makes a csv file with the new data and returns the
 # indices of this new data.
-def delete_users(path_dataset, min_num_items, num_users=1000):
+def delete_users(path_dataset, min_num_items, num_users=1000,
+                 save_path='preprocessed_data.csv'):
     """eliminates the inactive users from the original dataset.
     Args:
         * min_num_items:
             all users we keep should have rated at least min_num_items item.
+        * save_path:
+            path of output file
     Returns:
         * a csv file of the preprocesses data in the same format with
             the original data - saved as preprocessed_data.csv
@@ -88,42 +91,33 @@ def delete_users(path_dataset, min_num_items, num_users=1000):
             to the original data.
     """
 
-    def read_txt(path):
-        with open(path, "r") as f:
-            return f.read().splitlines()
+    # Loading Data
     data = read_txt(path_dataset)[1:]
-
-    def deal_line(line):
-        pos, rating = line.split(',')
-        row, col = pos.split("_")
-        row = row.replace("r", "")
-        col = col.replace("c", "")
-        return int(row), int(col), float(rating)
-
     data2 = [deal_line(line) for line in data]
+
+    # Counting number of movies wathced by a person
     cnt_items_per_user = np.zeros(num_users)
     for row, col, rating in data2:
         cnt_items_per_user[col-1] += 1
 
+    # Generating cleaned data set: label==-1 <-> the user is removed
     data_new = []
     labels = (-1) * np.ones(len(data))
 
     cnt_deleted = 0
     for i in range(len(data)):
-        ind1 = data[i].find('c')
-        ind2 = data[i].find(',')
-        if(cnt_items_per_user[int(data[i][ind1+1:ind2])-1] < min_num_items):
+        if(cnt_items_per_user[data2[i][1]-1] < min_num_items):
             cnt_deleted += 1
         else:
             data_new.append(data[i])
             labels[i] = int(i - cnt_deleted)
 
-    with open('preprocessed_data.csv', mode='w') as csv_file:
-        fieldnames = ['Id', 'Prediction']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    # Save Output
+    file = open(save_path, "w")
+    file.write("Id,Pediction\n")
 
-    writer.writeheader()
-    for i in range(len(data)):
-        writer.writerow({'Id': data_new[i][0:-2], 'Prediction': data_new[i]})
+    for line in data_new:
+        file.write(line + "\n")
 
+    file.close()
     return labels
